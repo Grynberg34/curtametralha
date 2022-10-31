29425ccf64cf9931ec45058fed49d7927c194a88
Francisco Bignotto <franciscogrynberg34@gmail.com>
2022-10-31 16:10:41 -0300

    v0.0.51

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,7 @@
 import Home from './Home';
-import ReactGA from "react-ga";
-import {useEffect } from 'react';
-
-const TRACKING_ID = "G-PVWM04C4C7";
-ReactGA.initialize(TRACKING_ID);
 
 
 function App() {
-
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
 
   return (
     <div>
