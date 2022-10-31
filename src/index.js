300c68585b230b60d2c7e2937259c67ed7af2d67
Francisco Bignotto <franciscogrynberg34@gmail.com>
2022-10-31 15:27:54 -0300

    v0.0.48

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import ReactGA from "react-ga";
 
 const TRACKING_ID = "G-PVWM04C4C7";
 ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 
 ReactDOM.render(
